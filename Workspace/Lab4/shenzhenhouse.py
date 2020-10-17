8cf559e3fe93f0856aebeb981c506d45d64e3986
faker <skt.faker.ccj@gmail.com>
2020-10-17 23:54:44 +0800

    ML

--- Workspace/Lab4/shenzhenhouse.py
+++ Workspace/Lab4/shenzhenhouse.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from pylab import mpl
 
 
 def read_data(filename):
@@ -44,7 +46,27 @@ def draw_box(data):
     plt.boxplot([yantian, nanshan, baoan, longgang, longhua, futian, luohu], labels=labels)
     plt.show()
 
+def draw_heatmap(data, index, columns, values):
+    pt = data.pivot_table(index=index, columns=columns, values=values, aggfunc=np.sum)
+    print(pt)
+    f, (ax) = plt.subplots(figsize=(6,6),nrows=1)
+    cmap = sns.cubehelix_palette(start = 1.5, rot = 3, gamma=0.8, as_cmap = True)
+    sns.heatmap(pt, linewidths = 0.05, ax=ax, vmax=900, vmin=0, cmap=cmap)
+    plt.show()
+
+
+def draw_hist2(data):
+    fig, (ax0, ax1) = plt.subplots(nrows=2, figsize=(9, 6))
+    x = data["总价"]
+    ax0.hist(x,100,normed=1,histtype='bar',facecolor='yellowgreen',alpha=0.75) 
+    ax1.hist(x,20,normed=1,histtype='bar',facecolor='pink',alpha=0.75,cumulative=True,rwidth=0.8)
+    plt.show()
+
 if __name__ == "__main__":
     data = read_data("./data/shenzhenhouse.csv")
     # draw_hist(data)
-    draw_box(data)
+    # draw_box(data)
+    # draw_heatmap(data, "区", "朝向", "总价")
+    draw_hist2(data)
+    
+
